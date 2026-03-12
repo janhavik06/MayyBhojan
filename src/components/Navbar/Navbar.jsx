2e86a86a2baa56c2a58bac77494576931337be11
janhavik06 <janhavikundgir06@gmail.com>
2026-03-12 15:06:20 +0530

    update authentication

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -19,6 +19,19 @@ export default function Navbar({ language, setLanguage, loggedIn }) {
 
   const user = JSON.parse(localStorage.getItem("maybhojan_user"));
   const role = user?.role;
+
+  function goToProfile() {
+    if (role === "cook") {
+      navigate("/cook");
+    } else if (role === "customer") {
+      navigate("/profile");
+    } else if (role === "delivery") {
+      navigate("/delivery");
+    } else if (role === "admin") {
+      navigate("/admin");
+    }
+  }
+
   return (
     <header className="w-full bg-white shadow-md shadow-black/5 sticky top-0 z-100">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 py-4 flex items-center justify-between">
@@ -101,13 +114,15 @@ export default function Navbar({ language, setLanguage, loggedIn }) {
             role !== "admin" &&
             role !== "delivery" && (
               <>
-                <Link to="/cart" className="relative text-xl cursor-pointer">
-                  <i className="fa-solid fa-basket-shopping"></i>
+                {role !== "cook" && (
+                  <Link to="/cart" className="relative text-xl cursor-pointer">
+                    <i className="fa-solid fa-basket-shopping"></i>
 
-                  <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs w-5 h-5 rounded-full flex items-center justify-center">
-                    {count}
-                  </span>
-                </Link>
+                    <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs w-5 h-5 rounded-full flex items-center justify-center">
+                      {count}
+                    </span>
+                  </Link>
+                )}
 
                 <div onClick={goToProfile} className="cursor-pointer">
                   <div className="w-10 h-10 rounded-full bg-gradient-to-r from-orange-400 to-orange-600 flex items-center justify-center text-white font-semibold shadow-md hover:shadow-lg transition">
@@ -188,15 +203,4 @@ export default function Navbar({ language, setLanguage, loggedIn }) {
       )}
     </header>
   );
-}
-function goToProfile() {
-  if (role === "cook") {
-    navigate("/cook");
-  } else if (role === "customer") {
-    navigate("/profile");
-  } else if (role === "delivery") {
-    navigate("/delivery");
-  } else if (role === "admin") {
-    navigate("/admin");
-  }
 }
